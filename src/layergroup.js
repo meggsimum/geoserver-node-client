6fdccf124846714b8c743a1c3b4ce5223c0c0361
blitza@terrrestris.de <blitza@terrestris.de>
2023-10-23 13:33:09 +0200

    Update layergroup jsdoc

--- src/layergroup.js
+++ src/layergroup.js
@@ -3,13 +3,13 @@ import { getGeoServerResponseText, GeoServerResponseError } from './util/geoserv
 import AboutClient from './about.js'
 
 /**
- * Client for GeoServer layers
+ * Client for GeoServer layergroups
  *
  * @module LayerClient
  */
 export default class LayerGroupClient {
   /**
-   * Creates a GeoServer REST LayerClient instance.
+   * Creates a GeoServer REST LayerGroupClient instance.
    *
    * @param {String} url The URL of the GeoServer REST API endpoint
    * @param {String} auth The Basic Authentication string
@@ -21,7 +21,7 @@ export default class LayerGroupClient {
 
   /**
    * Returns a GeoServer layergroup by the given workspace and layergroup name,
-   * e.g. "myWs:myLayer".
+   * e.g. "myWs:myLayergroup".
    *
    * @param {String} workspace The name of the workspace
    * @param {String} layerGroupName The name of the layer to query
@@ -54,7 +54,16 @@ export default class LayerGroupClient {
     return response.json();
   }
 
-  async modify (workspace, layerGroupName, jsonBody) {
+  /**
+   * Modifies an existing GeoServer layergroup
+   *
+   * @param {String} workspace The name of the workspace
+   * @param {String} layerName The name of the layergroup to modify
+   * @param {Object} layerGroupDefinition The modified definiton of the layergroup
+   *
+   * @throws Error if request fails
+   */
+  async modify (workspace, layerGroupName, layerGroupDefinition) {
     const url = `${this.url}/workspaces/${workspace}/layergroups/${layerGroupName}.json`;
     const response = await fetch(url, {
       credentials: 'include',
@@ -63,7 +72,7 @@ export default class LayerGroupClient {
         Authorization: this.auth,
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(jsonBody)
+      body: JSON.stringify(layerGroupDefinition)
     });
 
     if (!response.ok) {
